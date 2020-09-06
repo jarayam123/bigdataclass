82636c7b4cef4d0d35fc4d00a0acfd91579fcdcc
jarayam123 <68527396+jarayam123@users.noreply.github.com>
2020-09-06 16:16:50 -0600

    Update joiner.py

--- testsample/joiner.py
+++ testsample/joiner.py
@@ -1,4 +1,10 @@
 """Toy join function to showcase spark functions."""
 
 def join_dataframes(left, right, columns_left, columns_right):
-    pass
+    where = []
+    for column in range(len(columns_left)):
+        lc = left[columns_left[column]]
+        rc = right[columns_right[column]]
+        where.append(lc==rc)
+
+    return left.join(right, where)
